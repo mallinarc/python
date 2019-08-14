5642a3fd70b1f65c59064930627914ba01a10219
mallinarc <mallinarc@hotmail.com>
2019-08-14 19:08:06 -0400

    updated with tutorial solutions

--- beginning.py
+++ beginning.py
@@ -10,3 +10,18 @@ print(dblQuoteString)
 # single quote without string termination slash
 anotherString = 'Ramesh\'s blog.'
 print(anotherString)
+# this will not work
+one = '1'
+two = '2'
+hello = 'hello'
+print(one+two+hello)
+
+myString = 'hello'
+myFloat = 10.0
+myInt = 20
+if myString == 'hello':
+    print("String: %s" % myString)
+if isinstance(myFloat, float) and myFloat == 10.0:
+    print("Float %f" % myFloat)
+if isinstance(myInt, int) and myInt == 20:
+    print("Integer: %d" % myInt)
