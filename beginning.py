4ef3a7c12ca0db152d643fa915ae9c037a2c051e
mallinarc <mallinarc@hotmail.com>
2019-08-14 15:25:36 -0400

    Initial commit.

---
+++ beginning.py
@@ -0,0 +1,12 @@
+x = 1
+if x == 1:
+    # indent four spaces
+    print("x is 1:")
+
+thisString = 'Hello'
+print(thisString)
+dblQuoteString = "Ramesh's blog."
+print(dblQuoteString)
+# single quote without string termination slash
+anotherString = 'Ramesh\'s blog.'
+print(anotherString)
